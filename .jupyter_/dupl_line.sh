d02ac6f6f8f8a783b5fe1c5b9a0bd18f21701118
a-chern <alena.chernenko@essenceglobal.com>
2022-03-16 15:39:28 -0400

    Update dupl_line.sh
    adjusted paths

--- .jupyter_/dupl_line.sh
+++ .jupyter_/dupl_line.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
  
-# Appends a js script for duplicating a line in juper notebook to ~/.jupyter/custom/custom.js
-cat ~/mnt/.jupyter/custom2 >> ~/.jupyter/custom/custom.js
+# Append a js script for duplicating a line in juper notebook to ~/.jupyter/custom/custom.js
+cat ~/.jupyter_/custom2 >> ~/.jupyter/custom/custom.js
+
+# (Optional) Remove ~/.jupyter_ folder
+#rm -r ~/.jupyter_
